1c00bb01c2fffd5397ff2d00a0c5308cba16441f
Liberbell <liberbell@gmail.com>
2020-08-27 08:59:43 +0900

    sample5

---
+++ helloworld/Shared/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  Shared
+//
+//  Created by hideaki ehara on 2020/08/27.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
